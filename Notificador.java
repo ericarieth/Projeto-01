563b5e4cf4e729010352fc05c08e65a27be94927
ericarieth <ericarieth@gmail.com>
2026-08-20 16:16:16 -0300

    Criar Notificador.java
    Criar classe contexto Notificador

---
+++ Notificador.java
@@ -0,0 +1,11 @@
+public class Notificador (
+    
+    private NotificacaoStrategy estrategia;
+
+    public Notificador (NotificacaoStrategy estrategia) (
+    this. estrategia - estrategia;
+
+    public void enviarMensagem(String mensagem) {
+    estrategia.enviar (mensagem);
+    }
+}
