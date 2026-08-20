3c91da93255451cabe138ce95326f70f0c90302b
ericarieth <ericarieth@gmail.com>
2026-08-20 16:28:00 -0300

    Criar Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+        Notificador email =
+        new Notificador(new EmailNotificacao());
+ 
+        email.enviarMensagem("Bem-vindo ao sistema!");
+ 
+        Notificador sms =
+        new Notificador(new SmsNotificacao());
+ 
+        sms.enviarMensagem("Seu código foi aprovado!");
+ 
+        Notificador whatsapp =
+        new Notificador(new WhatsAppNotificacao());
+ 
+        whatsapp.enviarMensagem("Pagamento realizado!");
+    }
+}
