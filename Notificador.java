107e0a8df7c7f3363700de86032e0abf62b01d07
ericarieth <ericarieth@gmail.com>
2026-08-20 16:16:59 -0300

    Atualizar o Notificador.java

--- Notificador.java
+++ Notificador.java
@@ -2,9 +2,9 @@ public class Notificador (
     
     private NotificacaoStrategy estrategia;
 
-    public Notificador (NotificacaoStrategy estrategia) (
+    public Notificador (NotificacaoStrategy estrategia) {
     this. estrategia - estrategia;
-
+    }
     public void enviarMensagem(String mensagem) {
     estrategia.enviar (mensagem);
     }
